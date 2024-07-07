bead02e6854857090ea6ecf30b1f80ad76736d28
Margarita Shishkova <margolen239@gmail.com>
2024-07-07 20:52:59 +0100

    Create product page

--- src/components/organisms/product/product.tsx
+++ src/components/organisms/product/product.tsx
@@ -1,22 +1,59 @@
+import itemImage from '../../../assets/images/item.png';
+import { Button } from '../../atoms/button/button';
+import { Container } from '../../templates/container/container';
+
 import styles from './product.module.css';
 
 export function Product() {
+  const items = [...Array(6)];
+
   return (
-    <div className={styles.product__container}>
-      <div className={styles.product__gallery}></div>
-      <div className={styles.product__info}>
-        <p className={styles.product__title}></p>
-        <p className={styles.product__meta}>
-          <p className={styles.product__score}>
-            <p className={styles.stars}>
-              <span className={styles.scale}></span>
-              <span className={styles.rating}></span>
-              <span className={styles.hidden}>Rating: 4 out of 5 stars</span>
+    <Container>
+      <div className={styles.product__container}>
+        <div className={styles.product__images}>
+          <img src={itemImage} alt="item" />
+          <div className={styles.product__gallery}>
+            {items.map((_, itemId) => (
+              <img key={itemId} src={itemImage} alt="item" />
+            ))}
+          </div>
+        </div>
+        <div className={styles.product__info}>
+          <h1 className={styles.product__title}>Essence Mascara Lash Princess</h1>
+          <p className={styles.product__meta}>
+            <p className={styles.product__score}>
+              <p className={styles.stars}>
+                <span className={styles.scale}></span>
+                <span className={styles.rating}></span>
+                <span className={styles.hidden}>Rating: 4 out of 5 stars</span>
+              </p>
             </p>
+            <p className={styles.product__categories}>electronics, selfie accessories</p>
           </p>
-          <p className={styles.product__categories}></p>
-        </p>
+          <p className={styles.product__availability}>In Stock - Only 5 left!</p>
+          <p className={styles.product__description}>
+            The Essence Mascara Lash Princess is a popular mascara known for its volumizing and
+            lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free
+            formula.
+          </p>
+          <p className={styles.product__other_info}>
+            <p>1 month warranty </p>
+            <p>Ships in 1 month</p>
+          </p>
+          <p className={styles.product__buy}>
+            <p className={styles.product__price}>
+              <p className={styles.product__price_info}>
+                <p>7.17$</p>
+                <p>9.99$</p>
+              </p>
+              <p>
+                Your discount: <b>14.5%</b>
+              </p>
+            </p>
+            <Button className={styles.product__add}>Add to cart</Button>
+          </p>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
--- src/components/pages/homePage/homePage.tsx
+++ src/components/pages/homePage/homePage.tsx
@@ -2,12 +2,14 @@ import { Greeting } from '../../organisms/greeting/greeting';
 import { Catalog } from '../../organisms/catalog/catalog';
 import { FAQ } from '../../organisms/faq/faq';
 
+import styles from './homePage.module.css';
+
 export function HomePage() {
   return (
-    <>
+    <div className={styles.home}>
       <Greeting />
       <Catalog />
       <FAQ />
-    </>
+    </div>
   );
 }
