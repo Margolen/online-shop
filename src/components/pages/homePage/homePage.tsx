2ff4e41a13394f91114fbb08a6ce2f69d248b492
Margarita Shishkova <margolen239@gmail.com>
2024-07-07 21:27:44 +0100

    Fix white bar

--- src/components/pages/homePage/homePage.tsx
+++ src/components/pages/homePage/homePage.tsx
@@ -1,12 +1,18 @@
 import { Greeting } from '../../organisms/greeting/greeting';
 import { Catalog } from '../../organisms/catalog/catalog';
 import { FAQ } from '../../organisms/faq/faq';
+import { Container } from '../../templates/container/container';
 
 import styles from './homePage.module.css';
 
 export function HomePage() {
   return (
-    <div className={styles.home}>
+    <div>
+      <div className={styles.bar__container}>
+        <Container>
+          <div className={styles.bar}></div>
+        </Container>
+      </div>
       <Greeting />
       <Catalog />
       <FAQ />
