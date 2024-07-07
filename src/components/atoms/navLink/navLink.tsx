25805ef770501245cfc9b538291dc5813ccb04a2
Margarita Shishkova <margolen239@gmail.com>
2024-07-07 15:18:35 +0100

    Add hover effect for nav links

--- src/components/atoms/navLink/navLink.tsx
+++ src/components/atoms/navLink/navLink.tsx
@@ -1,9 +1,9 @@
-import './navLink.module.css';
+import styles from './navLink.module.css';
 
 export interface NavLinkProps {
   children: React.ReactNode;
 }
 
 export function NavLink({ children }: NavLinkProps) {
-  return <a>{children}</a>;
+  return <a className={styles.nav}>{children}</a>;
 }
