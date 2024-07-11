57727a7b7a7f883aa610afd5a28300350df1f62b
Margarita Shishkova <margolen239@gmail.com>
2024-07-11 17:29:17 +0100

    Finish MyCart page

--- src/components/organisms/myCart/myCart.tsx
+++ src/components/organisms/myCart/myCart.tsx
@@ -7,17 +7,30 @@ export function MyCart() {
   const items = [1, 1, 5];
 
   return (
-    <Container>
+    <div className={styles.cart__wrapper}>
       <h1>My Cart</h1>
-      <div className={styles.cart__wrapper}>
+      <div className={styles.cart__inner}>
         <div className={styles.cart__content}>
           {items.map((item, itemId) => (
             <CartItem key={itemId} itemId={itemId} itemCounter={item} />
           ))}
           <CartItem itemId={100} itemCounter={0} />
         </div>
-        <div className={styles.cart__price}></div>
+        <div className={styles.cart__price}>
+          <div className={styles.cart__price_count}>
+            <span className={styles.cart__price_count_text}>Total count</span>
+            <span className={styles.cart__price_count_number}>3 items</span>
+          </div>
+          <div className={styles.cart__price_without_discount}>
+            <span className={styles.cart__price_count_text}>Price without discount</span>
+            <span className={styles.cart__price_count_number}>700$</span>
+          </div>
+          <div className={styles.cart__price_total}>
+            <span className={styles.cart__price_count_text}>Total price</span>
+            <span className={styles.cart__price_count_number}>590$</span>
+          </div>
+        </div>
       </div>
-    </Container>
+    </div>
   );
 }
