a60a1d7caa7a60829935fd84814bf4fc0617a1ba
Margarita Shishkova <margolen239@gmail.com>
2024-07-11 11:00:45 +0100

    Create ButtonPanel component

---
+++ src/components/molecules/buttonPanel/buttonPanel.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from '../../atoms/button/button';
+
+import cartIcon from '../../../assets/images/icons/cart.svg';
+import minusIcon from '../../../assets/images/icons/minus.svg';
+import plusIcon from '../../../assets/images/icons/plus.svg';
+
+import styles from './buttonPanel.module.css';
+
+export interface ButtonPanelProps {
+  onHovered?: (status: boolean) => void;
+}
+
+export function ButtonPanel({ onHovered }: ButtonPanelProps) {
+  const [itemCounter, setItemCounter] = useState(0);
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    if (onHovered) {
+      onHovered(hovered);
+    }
+  }, [hovered]);
+
+  return (
+    <span
+      className={styles.card__button_panel}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {itemCounter === 0 ? (
+        <Button
+          className={styles.card__button}
+          onClick={() => setItemCounter((prevCounter) => prevCounter + 1)}
+        >
+          <img src={cartIcon} alt="cart" />
+        </Button>
+      ) : (
+        <>
+          <Button
+            className={styles.card__button}
+            onClick={() => setItemCounter((prevCounter) => prevCounter - 1)}
+          >
+            <img src={minusIcon} alt="minus" />
+          </Button>
+          <span className={styles.card__button_counter}>
+            {itemCounter} {itemCounter > 1 ? 'items' : 'item'}
+          </span>
+          <Button
+            className={styles.card__button}
+            onClick={() => setItemCounter((prevCounter) => prevCounter + 1)}
+          >
+            <img src={plusIcon} alt="plus" />
+          </Button>
+        </>
+      )}
+    </span>
+  );
+}
